b3b24d8acc64b9be657c742535c35a597838e768
htbtran9x <46492546+htbtran9x@users.noreply.github.com>
2022-11-16 23:29:24 +0700

    init

---
+++ after/plugin/treesitter.rc.lua
@@ -0,0 +1,22 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = { "lua" },
+    additional_vim_regex_highlighting = true,
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = "all",
+  autotag = {
+    enable = true,
+  },
+  sync_install = false,
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
